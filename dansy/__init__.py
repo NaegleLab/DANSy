5e34d6073d34bb48d2b1b73ed72622e610236720
adshimpi <138716159+adshimpi@users.noreply.github.com>
2025-07-25 15:45:34 -0400

    Updated __init__ file

--- dansy/__init__.py
+++ dansy/__init__.py
@@ -1,7 +1,6 @@
 from .dansy import *
 from .deDANSy import *
 from .network_separation_helpers import *
-
 from .generateCompleteProteome import import_proteome_files
 
 __all__ = ["dansy", "deDANSy", "network_separation_helpers", "import_proteome_files"]
