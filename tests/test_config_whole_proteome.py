37f37693c47ef2fd3e57145c80701fec83f08bdc
adshimpi <138716159+adshimpi@users.noreply.github.com>
2025-10-07 13:24:17 -0400

    Updated the test file for the config file to check the proteome version updating method.

--- tests/test_config_whole_proteome.py
+++ tests/test_config_whole_proteome.py
@@ -13,6 +13,9 @@ def main():
     ref = dansy.helper.import_proteome_files()
     assert isinstance(ref, pd.DataFrame), "Issue with the reference file"
 
+    original_version = dansy.config.DANSY_PROTEOME_VERSION
+    assert original_version == '20250512.csv', "Leftover version update"
+
     # Try to update the ref and make sure we get an error when trying to import an improper version
     new_version = '20251010.csv'
     try:
@@ -22,8 +25,18 @@ def main():
     
     assert not ref.equals(new_ref), "Issue with importing a version that shouldn't work"
 
-    dansy.config.update_proteome_version(new_version)
-    assert dansy.config.DANSY_PROTEOME_VERSION == new_version, "Version updating function not working as intended."
+    dansy.config.update_proteome_version(new_version, default=False)
+
+    version_file = f"{dansy.config.DANSY_DIR}/dansy/current_proteome_version.txt"
+    prot_ver = []
+    with open(version_file, 'r') as f:
+        for line in f:
+            prot_ver.append(line)
+    current_version = prot_ver[0]
+    assert current_version == original_version, "The default value was updated when it was not intended."
+    assert dansy.config.DANSY_PROTEOME_VERSION == new_version, "Proteome version did not get updated."
+
+    dansy.config.update_proteome_version(original_version, default=True)
 
     # Now let's make sure we can still create a DANSy object from a small subset without providing the reference file
     test_ref = pd.read_csv('tests/test_data/small_tyrkin_reference.csv')
@@ -33,7 +46,6 @@ def main():
     assert wp.protsOI == protsOI, "Issue with proteins of interest."
     # Since this uses the reference file from the whole proteome it should be just slightly different (mostly the indices) than our smaller version.
     assert not wp.ref.equals(test_ref), "The reference file that was imported for the dansy object was identical to a smaller reference file"
-
 
 if __name__ == '__main__':
     main()
